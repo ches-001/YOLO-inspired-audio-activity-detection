63fa76ebff7dc6650620ece33ffb870313cf6a85
Darkshades <henrychibueze774@gmail.com>
2024-07-19 15:09:30 +0100

    push

--- modules/_loss.py
+++ modules/_loss.py
@@ -98,7 +98,7 @@ class AudioDetectionLoss(nn.Module):
         # Its IoU to the target as its confidence, this can also theoretically make for a good
         # regularisation technique, akin to label smoothening.
         if self.iou_confidence:
-            obj_target_confidence = (obj_target_confidence * ious_max.unsqueeze(-1)).detach()
+            obj_target_confidence = (obj_target_confidence * ious_max.unsqueeze(-1)).detach().clip(min=0)
         pos_mask = obj_target_confidence > 0
         num_neg = obj_target_confidence[torch.bitwise_not(pos_mask)].shape[0] + noobj_thresholding_mask.sum()
         num_pos = obj_target_confidence[pos_mask].shape[0]
