770851fd4eb807c97e11f1e76ecd06591ed36e14
Darkshades <henrychibueze774@gmail.com>
2024-07-20 22:57:06 +0100

    ReLU name correction

--- modules/_common.py
+++ modules/_common.py
@@ -12,7 +12,7 @@ class ConvBNorm(nn.Module):
             kernel_size: Union[int, Tuple[int, int]], 
             stride: Union[int, Tuple[int, int]]=1, 
             padding: Optional[Union[int, Tuple[int, int]]]=None,
-            activation: Optional[Type]=nn.RELU,
+            activation: Optional[Type]=nn.ReLU,
             bias: bool=True,
         ):
         super(ConvBNorm, self).__init__()
@@ -46,7 +46,7 @@ class RepVGGBlock(nn.Module):
             self, 
             in_channels: int, 
             out_channels: int, 
-            activation: Optional[Type]=nn.RELU, 
+            activation: Optional[Type]=nn.ReLU, 
             stride: Union[int, Tuple[int, int]]=1,
             padding: Optional[Union[int, Tuple[int, int]]]=None,
         ):
