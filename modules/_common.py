76115b68ee38944d5379e8199db4e4f5498ff4a3
Darkshades <henrychibueze774@gmail.com>
2024-07-20 22:56:22 +0100

    GELU to RELU

--- modules/_common.py
+++ modules/_common.py
@@ -12,7 +12,7 @@ class ConvBNorm(nn.Module):
             kernel_size: Union[int, Tuple[int, int]], 
             stride: Union[int, Tuple[int, int]]=1, 
             padding: Optional[Union[int, Tuple[int, int]]]=None,
-            activation: Optional[Type]=nn.GELU,
+            activation: Optional[Type]=nn.RELU,
             bias: bool=True,
         ):
         super(ConvBNorm, self).__init__()
@@ -46,7 +46,7 @@ class RepVGGBlock(nn.Module):
             self, 
             in_channels: int, 
             out_channels: int, 
-            activation: Optional[Type]=nn.GELU, 
+            activation: Optional[Type]=nn.RELU, 
             stride: Union[int, Tuple[int, int]]=1,
             padding: Optional[Union[int, Tuple[int, int]]]=None,
         ):
